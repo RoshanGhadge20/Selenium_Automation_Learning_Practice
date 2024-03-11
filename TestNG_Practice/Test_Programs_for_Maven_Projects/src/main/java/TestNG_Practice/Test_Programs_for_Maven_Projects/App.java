00103dd957020575295315756641e58222d566fc
RoshanGhadge20 <roshanghadge20@gmail.com>
2024-03-11 09:16:46 +0530

    Removed embedded repository

---
+++ TestNG_Practice/Test_Programs_for_Maven_Projects/src/main/java/TestNG_Practice/Test_Programs_for_Maven_Projects/App.java
@@ -0,0 +1,13 @@
+package TestNG_Practice.Test_Programs_for_Maven_Projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestNG_Practice/Test_Programs_for_Maven_Projects/src/test/java/Programs/Working_with_website_methods.java
@@ -0,0 +1,10 @@
+package Programs;
+
+
+public class Working_with_website_methods {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
